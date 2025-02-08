42f4ab206c302a1faf166c5cbb392866ba39460c
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2025-02-07 21:54:26 -0700

    create a protocol for a chat server

---
+++ chat/lib/chat/messages.ex
@@ -0,0 +1,12 @@
+defmodule Chat.Message do
+  defmodule Register do
+    @type t() :: %__MODULE__{username: String.t()}
+    defstruct [:username]
+  end
+
+  defmodule Broadcast do
+    @type t() :: %__MODULE__{from_username: String.t(), contents: STring.t()}
+
+    defstruct [:from_username, :contents]
+  end
+end
